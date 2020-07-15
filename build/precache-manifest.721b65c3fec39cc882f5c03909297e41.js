e5d8e520ef10a4565b057e034f8e8c5f4bfde3ec
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2020-07-15 13:58:38 -0300

    intento 80123

---
+++ build/precache-manifest.721b65c3fec39cc882f5c03909297e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0fbecac8c5bcfb3c7613f1f92b2a876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd80b9efb3b41a72626",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cca55e8e0634ff2b5146",
+    "url": "/static/js/2.6bf0154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bf0154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd80b9efb3b41a72626",
+    "url": "/static/js/main.aa7ea45b.chunk.js"
+  },
+  {
+    "revision": "60334080a3128d4a2926",
+    "url": "/static/js/runtime-main.d32eecf1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
